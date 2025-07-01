ae50e98ebb621b5a0f508f6b348c63692b25ce43
Anthony Aniobi <anthonyaniobi198@gmail.com>
2025-07-01 13:48:27 +0100

    add images and fonts

---
+++ lib/constants/app_images.dart
@@ -0,0 +1,34 @@
+class AppImages {
+  static const String orderComplete = 'assets/images/order_complete.png';
+}
+
+class AppSvg {
+  static const String arrowDown = 'assets/icons/arrow_down.svg';
+  static const String arrowLeft = 'assets/icons/arrow-left.svg';
+  static const String bagActive = 'assets/icons/bag_active.svg';
+  static const String bagFilled = 'assets/icons/bag_filled.svg';
+  static const String bagInactive = 'assets/icons/bag_inactive.svg';
+  static const String call = 'assets/icons/call.svg';
+  static const String card = 'assets/icons/card.svg';
+  static const String close = 'assets/icons/close.svg';
+  static const String dispatchFilled = 'assets/icons/dispatch_filled.svg';
+  static const String dispatchedOutlined =
+      'assets/icons/dispatched_outlined.svg';
+  static const String dropFilled = 'assets/icons/drop_filled.svg';
+  static const String dropOutlined = 'assets/icons/drop_outlined.svg';
+  static const String frame = 'assets/icons/frame.svg';
+  static const String homeActive = 'assets/icons/home_active.svg';
+  static const String homeInactive = 'assets/icons/home_inactive.svg';
+  static const String horizontalArrow = 'assets/icons/horizontal_arrow.svg';
+  static const String locationMinus = 'assets/icons/location-minus.svg';
+  static const String logout = 'assets/icons/logout.svg';
+  static const String ordersActive = 'assets/icons/orders_active.svg';
+  static const String ordersInactive = 'assets/icons/orders_inactive.svg';
+  static const String profileActive = 'assets/icons/profile_active.svg';
+  static const String profileInactive = 'assets/icons/profile_inactive.svg';
+  static const String searchNormal = 'assets/icons/search-normal.svg';
+  static const String send = 'assets/icons/send.svg';
+  static const String setting = 'assets/icons/setting.svg';
+  static const String star = 'assets/icons/star.svg';
+  static const String timer = 'assets/icons/timer.svg';
+}
